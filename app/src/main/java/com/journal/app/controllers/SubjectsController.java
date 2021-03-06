fd693ff2fdbe7df7fac6c9abb042d05cef492980
DvynilHub <belonogov220@gmail.com>
2021-03-06 21:19:36 +0800

    вывод групп преподавателя(пока не работает)

---
+++ app/src/main/java/com/journal/app/controllers/SubjectsController.java
@@ -0,0 +1,57 @@
+package com.journal.app.controllers;
+
+import com.journal.app.models.Lesson;
+import com.journal.app.models.Teacher;
+import com.journal.app.repositories.LessonsRepository;
+import com.journal.app.repositories.TeachersRepository;
+import com.journal.app.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SubjectsController {
+
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Autowired
+    TeachersRepository teachersRepository;
+
+    @Autowired
+    LessonsRepository lessonsRepository;
+
+    /** Метод перехода на страницу предметов преподавателя
+     * @see Model
+     * @see Authentication
+     * @see Teacher
+     * @param model
+     * @return subjects page
+     */
+    @RequestMapping("/journal/subjects")
+    public String getSubjects(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        Teacher teacher = teachersRepository.findTeacherByLogin(name);
+        Long id = teacher.getTeacherId();
+        Iterable<Lesson> lessons = lessonsRepository.getLessons(id);
+        model.addAttribute("lessons", lessons);
+        model.addAttribute("teacher",teacher);
+        return "subjects";
+    }
+
+    @RequestMapping("/subject/{id}")
+    public String getSubjectById(@PathVariable("id") Long id, Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        Teacher teacher = teachersRepository.findTeacherByLogin(name);
+        model.addAttribute("lesson", lessonsRepository.getOne(id));
+        model.addAttribute("teacher",teacher);
+        return "subject";
+    }
+
+}
