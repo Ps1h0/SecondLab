1fab4c9e15436c9885500bea7bb7d5e5bf5284c9
Ps1h0 <platonov.liceum@mail.ru>
2021-03-07 23:54:24 +0800

    Добавлена посещаемость

--- app/src/main/java/com/journal/app/controllers/AttendanceController.java
+++ app/src/main/java/com/journal/app/controllers/AttendanceController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,11 +21,12 @@ public class AttendanceController {
     @Autowired
     ScheduleRepository scheduleRepository;
 
-    @RequestMapping("/attendance")
-    public String attendancePage(Model model){
+    @RequestMapping("/attendance/{id}")
+    public String attendancePage(@PathVariable("id") Long id, Model model){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String name = auth.getName();
         Teacher teacher = teachersRepository.findTeacherByLogin(name);
+        model.addAttribute("lesson", lessonsRepository.getOne(id));
         model.addAttribute("teacher", teacher);
         return "attendance";
     }
